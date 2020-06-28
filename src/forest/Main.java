261c2c4c88ba64b2167d2979c2252a7ff699c308
AskaV <caya2321@gmail.com>
2020-06-28 23:42:12 +0300

    write in file

--- src/forest/Main.java
+++ src/forest/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
        // FileWriter file= new FileWriter("log11.txt");
         //file.write("hello");
-        //file.close();
+        //file.close();р
 
 
 //       for (int i = 0; i<10000;i++) {
